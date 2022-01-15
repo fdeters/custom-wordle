848285be3b2a012d2c9001d70b99f88c74023ba1
Forrest Deters <forrest.deters@gmail.com>
2022-01-15 12:31:08 -0800

    added keyboard

--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,40 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
+import Keyboard from 'react-simple-keyboard';
+import 'react-simple-keyboard/build/css/index.css';
 import './Game.css';
 
-function Tile () {
-  const [letter, setLetter] = useState(null);
+function Tile() {
+  const [letter, setLetter] = useState("");
 
   return (
-    <div className='game-tile'></div>
+    <div className='game-tile'>{letter}</div>
+  );
+}
+
+function GameKeyboard(props) {
+  const handleChange = (input) => {
+    console.log("Input changed: ", input);
+  }
+
+  const handleKeyPress = (button) => {
+    console.log("Button pressed: ", button);
+  }
+
+  return (
+    <Keyboard
+      layoutName={"default"}
+      onChange={handleChange}
+      onKeyPress={handleKeyPress}
+      layout={{
+        default: ["q w e r t y u i o p", "a s d f g h j k l", "{enter} z x c v b n m {bksp}"],
+      }}
+    />
   );
 }
 
 function Game() {
   return (
-    <Tile></Tile>
+    <GameKeyboard></GameKeyboard>
   );
 }
 
