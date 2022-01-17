0181aa8dcb771e8a7b7ea1818e57713eef0b8888
Forrest Deters <forrest.deters@gmail.com>
2022-01-16 17:34:51 -0800

    formatting/style changes

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import './style/Tile.css';
 
+const letterToUpperCase = (text) => {
+  return text === undefined ? "" : String(text).toUpperCase();
+}
+
 function Tile(props) {
     return (
-      <div className='game-tile'>{props.value}</div>
+      <div className='game-tile'>{letterToUpperCase(props.value)}</div>
     );
 }
 
